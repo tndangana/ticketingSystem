d21b7f6ba196c01b80f2037764a010d85a5789f3
Tonderai Ndangana <ndangana8@gmail.com>
2016-12-06 17:46:27 +0200

    add idbase entity and namebase entity to system

---
+++ ticketingSystem-business/src/main/java/zw/co/tonytech/ticketingsystem/business/domain/NameBaseEntity.java
@@ -0,0 +1,19 @@
+package zw.co.tonytech.ticketingsystem.business.domain;
+
+/**
+ * Created by tndangana on 12/6/16.
+ */
+public class NameBaseEntity {
+
+    private String name ;
+
+
+    public String getName(){
+        return name;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
